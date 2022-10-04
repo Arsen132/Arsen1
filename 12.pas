dd2436b6e1d02cbe0456ca42586470b711d8c227
Arsen132 <114969412+Arsen132@users.noreply.github.com>
2022-10-04 12:19:50 +0900

    Update and rename 12.py to 12.pas

---
+++ 12.pas
@@ -0,0 +1,21 @@
+program calcus;
+uses Math;
+var a,b,c:integer; 
+d:string;
+begin
+clrscr;
+writeln('чыhыла 1');
+readln(a);
+writeln('введите знак операции');
+readln(d);
+writeln('чыhыла 2');
+readln(c);
+if d=('+'), then b:=a+c;
+if d=('-'), then b:=a-c;
+if d=('*'), then b:=a*c;
+if d=('/'), then b:=a div c;
+if d=('**'), then b:=a*a;
+if d=('Sqrt'), then b:=Sqrt(a)
+writeln('эппиэт: ',b);
+readln;
+end.
